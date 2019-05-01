c382f51252f1f77e393b62b257a5f3c6ecb7f548
Serafim Nenarokov <serafim221@gmail.com>
2019-05-01 18:21:06 +0200

    Add changes

--- js/scripts.js
+++ js/scripts.js
@@ -33,91 +33,151 @@ function readTextFile(file){
   rawFile.send(null);
 }
 
-function Checkbox() {
-    var me = {
-        init: function ( svg, taxa, size, selected ) {
-            me.size = size;
-            me.svg = svg;
-            me.taxa = taxa;
-            me.box = null;
-            me.selected = selected;
+function TypeSelector() {
+  var me = {
+    MODES: [null, 'o', 'p', 'd'],
 
-            return me
-        },
+    init: function (svg, taxa, size, mode=null) {
+      if (mode != null){
+        mode = mode.replace(/\*/g, '');
+      }
 
-        draw: function ( sx, sy ) {
-            w = me.taxa.getBBox().width + 20;
-            h = me.taxa.getBBox().height / 2 - 5;
+      if (!me.MODES.includes(mode)){
+        console.log("error with mode: " + mode);
+      }
 
-            me.box = me.svg.rect(w, h, me.size, me.size);
-            me.box.attr({class: "checkmark", fill: "#bada55",});
+      me.size = size;
+      me.svg = svg;
+      me.taxa = taxa;
 
-            me.mark = me.svg.image("img/mark.svg", w, h, 10, 10);
+      me.ort_box = null;
+      me.par_box = null;
+      me.del_box = null;
 
-            me.shape = me.svg.group(me.box, me.mark);
-            me.taxa.after(me.shape);
+      me.mode = mode;
 
-            me.shape.click(function(){
-              me.toggle();
-            });
+      return me
+    },
 
-            me.redrawMark();
-        },
+    draw: function () {
+      w = me.taxa.getBBox().width + 5;
+      h = me.taxa.getBBox().height / 2 - 5;
 
-        redrawMark: function(){
-          if ( !me.selected ) {
-              me.mark.attr({opacity: 0});
-          } else {
-              me.mark.attr({opacity: 1});
-          }
-        },
-        on: function () {
-          me.selected = true;
-          me.redrawMark();
+      me.ort_box = me.svg.rect(w, h, me.size, me.size);
+      me.ort_box.attr({class: "checkmark", fill: "#a3ffb8",});
+      me.ort_mark = me.svg.image("img/o.svg", w+1, h-1, 10, 10);
+      me.ort_shape = me.svg.group(me.ort_box, me.ort_mark);
+      me.taxa.after(me.ort_shape);
 
-        },
-        off: function () {
-          me.selected = false;
-          me.redrawMark();
-        },
-        toggle: function () {
-            if ( !me.selected ) {
-                me.on();
+      w = w + me.ort_box.getBBox().width + 5;
+      me.par_box = me.svg.rect(w, h, me.size, me.size);
+      me.par_box.attr({class: "checkmark", fill: "#f1f49c",});
+      me.par_mark = me.svg.image("img/p.svg", w+1, h-1, 10, 10);
+      me.par_shape = me.svg.group(me.par_box, me.par_mark);
+      me.ort_shape.after(me.par_shape);
 
-            } else {
-                me.off();
-            }
-        }
+      w = w + me.par_box.getBBox().width + 5;
+      me.del_box = me.svg.rect(w, h, me.size, me.size);
+      me.del_box.attr({class: "checkmark", fill: "#f7b08a",});
+      me.del_mark = me.svg.image("img/d.svg", w+1, h-1, 10, 10);
+      me.del_shape = me.svg.group(me.del_box, me.del_mark);
+      me.par_shape.after(me.del_shape);
+
+      me.ort_shape.click(function(){
+        me.changeMode('o');
+      });
+
+      me.par_shape.click(function(){
+        me.changeMode('p');
+      });
+
+      me.del_shape.click(function(){
+        me.changeMode('d');
+      });
+
+      me.redrawMark();
+    },
+
+    redrawMark: function(){
+      me.ort_mark.attr({opacity: 0});
+      me.par_mark.attr({opacity: 0});
+      me.del_mark.attr({opacity: 0});
+
+      switch(me.mode){
+        case('o'):
+          me.ort_mark.attr({opacity: 1});
+          break;
+        case('p'):
+          me.par_mark.attr({opacity: 1});
+          break;
+        case('d'):
+          me.del_mark.attr({opacity: 1});
+          break;
+      }
+    },
+
+    changeMode: function(new_mode){
+      if (!me.MODES.includes(new_mode)){
+        console.log("error with mode: " + new_mode);
+      }
+
+      me.mode = new_mode;
+      me.redrawMark();
     }
+  }
 
-    return me;
-};
+  return me;
+  };
+
+function elementIsTaxa(el){
+  var text = el.attr("text");
+  return (!text.match(/^[\d.]+$/g) && !text.match(/^\</g) && !elementIsClass(el));
+}
+
+function elementIsClass(el){
+  return el.attr("text").match(/^\[/g);
+}
 
 function buildCsv(elements) {
   text = "";
+  var elementWithoutMode = null
   _.each(elements, function(val, key){
-    text += key + "," + val.checkbox.selected + "\n";
+    var mode = val.type_selector.mode;
+    if (elementWithoutMode == null && mode == null){
+      elementWithoutMode = val;
+      alert("You need to specify all the modes");
+    }
+    var klass = val.class.textContent.replace(/^\s+|\s+$/g, '');
+    text += key + "\t" + klass + "\t" + mode + "\n";
   });
+
+  if (elementWithoutMode != null){
+    return null;
+  }
+
   return text;
 }
 
 function applyCSV(data) {
+  // TODO
   var lines = data.split("\n");
-  _.each(lines, function(line){
-    line = line.replace(/^\s+|\s+$/g, '').split(',');
-    taxa = line[0];
-    val = (line[1] == "true");
 
-    if(elements[taxa] != undefined){
-      if(val){
-        elements[taxa].checkbox.on()
+  _.each(lines, function(line){
+    if (line.replace(/^\s+|\s+$/g, '') != ''){
+
+      line = line.replace(/^\s+|\s+$/g, '').split("\t");
+
+      taxa = line[0];
+      klass = line[1];
+      mode = line[2].replace(/\*/g, '');
+
+      if(elements[taxa] != undefined){
+        elements[taxa].type_selector.changeMode(mode);
       } else {
-        elements[taxa].checkbox.off()
+        console.log(taxa + " " + mode);
       }
     }
   });
-
-
 }
 
 function openSVG(raw_svg) {
@@ -126,17 +186,29 @@ function openSVG(raw_svg) {
 
   $("#svg").append(raw_svg);
   var svg = Snap("#svg > svg");
+  pdf = svg;
 
   _.each(svg.selectAll("text"), function(el){
     var taxa = el.attr("text");
-    if(!taxa.match(/^[\d.]+$/g) && !taxa.match(/^\[/g)){
-      var ch = new Checkbox().init(svg, el, 10, false);
+    if(elementIsTaxa(el)){
+      var ch = new TypeSelector().init(svg, el, 10);
       ch.draw();
-      elements[taxa] = { element: el,  checkbox: ch };
+
+      var klass = el.node.parentElement.nextElementSibling
+      elements[taxa] = { element: el, class: klass, type_selector: ch};
+      if (elm == null){
+        elm = el;
+      }
+      if(klass.textContent.replace(/^\s+|\s+$/g, '')[0] != "["){
+        // console.log(klass);
+      }
+    } else if (elementIsClass(el)){
+      el.animate({x: 200}, 100);
     }
   });
 };
-
+var pdf = null;
+var elm = null;
 var elements = {};
 
 $('#openTreeButton').on("click", function(modal_e){
@@ -181,5 +253,9 @@ $('#applyCSVFileButton').on("click", function(modal_e){
 
 $('#save-button').on("click", function(){
   text = buildCsv(elements);
-  download(text, "tree.csv");
+  if (text != null){
+    download(text, "tree.tsv");
+  }
 });
+
+
